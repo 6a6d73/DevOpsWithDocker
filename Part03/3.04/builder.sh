a4aa3c7fb5469f5c3900b846098a060142e9000e
Juan Serrano <9406503+6a6d73@users.noreply.github.com>
2023-06-17 15:00:58 +0300

    3.04 building images with dind

---
+++ Part03/3.04/builder.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+git_repo=${1}
+docker_repo=${2}
+dl_location="/tmp/$(uuidgen)"
+
+git clone ${git_repo} ${dl_location}
+
+docker build ${dl_location} --tag ${docker_repo}
+
+docker login -u ${DOCKER_USER} -p ${DOCKER_PWD}
+
+docker push ${docker_repo}
